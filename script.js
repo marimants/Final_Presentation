297ca88b5a1ab01f11fe42815206138136b52618
marimants <90420071+marimants@users.noreply.github.com>
2021-10-25 17:57:23 +0400

    done

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let navigation = document.getElementById('navi');
-let burgerIcon = document.getElementById('burger');
+// let burgerBarBlock = document.getElementsByClassName('icon-bar');
+// let burgerIcon = document.getElementById('burger-bar');
 
-burgerIcon.addEventListener('click', function() {
-    navigation.classList.toggle('active')
-})
+// burgerIcon.addEventListener('click', function() {
+//     burgerBarBlock.classList.toggle('active');
+// });
+
+
+// function myFunction(x) {
+//     x.classList.toggle("change");
+//   }
